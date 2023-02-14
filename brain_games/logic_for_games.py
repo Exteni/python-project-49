adbb918f05f6c8235fbbac73cc841e11695f0de2
Exteni <Extenit@yandex.ru>
2023-02-14 21:13:05 +0300

    настроил передачу модуля вместо 3-х аргументов

--- brain_games/logic_for_games.py
+++ brain_games/logic_for_games.py
@@ -1,14 +1,14 @@
 from brain_games.cli import welcome_user
 
 
-def main_game_logic(game_rules, question_for_user, answer_for_question):
+def main_game_logic(game):
     username = welcome_user()
-    print(game_rules)
+    print(game.game_rules)
     for _ in range(3):
-        question = question_for_user()
+        question = game.question_for_user()
         print(f"Question: {question}")
         user_answer = input("Your answer: ")
-        right_answer = answer_for_question(question)
+        right_answer = game.right_answer(question)
         if user_answer == right_answer:
             print("Correct!")
         else:
@@ -17,3 +17,4 @@ def main_game_logic(game_rules, question_for_user, answer_for_question):
             print(f"Let's try again, {username}!")
             return
     print(f"Congratulations, {username}!")
+    
